3dc06a76d9e4b957cd0a0a30fc43d814904d65a8
Samsul <rohmansamsul91@gmail.com>
2019-04-30 12:47:06 +0700

    edit form-input page

--- src/pages/form-input/form-input.ts
+++ src/pages/form-input/form-input.ts
@@ -1,25 +1,67 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the FormInputPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
+import { Component } from "@angular/core";
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  ToastController
+} from "ionic-angular";
+import { Validators, FormBuilder, FormGroup } from "@angular/forms";
+import { CommonProvider } from "../../providers/common/common";
+import { AuthServiceProvider } from "../../providers/auth-service/auth-service";
 @IonicPage()
 @Component({
-  selector: 'page-form-input',
-  templateUrl: 'form-input.html',
+  selector: "page-form-input",
+  templateUrl: "form-input.html"
 })
 export class FormInputPage {
+  public FormSimpanData: FormGroup;
+  responseInputDataBarang: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private formBuilder: FormBuilder,
+    public authService: AuthServiceProvider,
+    public common: CommonProvider,
+    public toastCtrl: ToastController
+  ) {
+    this.FormSimpanData = this.formBuilder.group({
+      nama_barang: [""],
+      qty: [""],
+      harga: [""]
+    });
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad FormInputPage');
+  simpan() {
+    this.common.presentLoading();
+    this.authService.postData(this.FormSimpanData.value, "Input_Barang").then(
+      result => {
+        this.responseInputDataBarang = result;
+        if (this.responseInputDataBarang.Input_Barang == "input success") {
+          const toast = this.toastCtrl.create({
+            message: "Data berhasil disimpan.",
+            duration: 2500
+          });
+          toast.present();
+          this.common.closeLoading();
+          this.navCtrl.pop();
+        } else {
+          const toast = this.toastCtrl.create({
+            message: "Data gagal disimpan.",
+            duration: 2500
+          });
+          toast.present();
+          this.common.closeLoading();
+        }
+      },
+      err => {
+        const toast = this.toastCtrl.create({
+          message: "Gagal koneksi ke server.",
+          duration: 2500
+        });
+        toast.present();
+        this.common.closeLoading();
+      }
+    );
   }
-
 }
